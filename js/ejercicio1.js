90642ce9df9425d7300dcbbdf9e53695343c3306
David Palau Morant <palau.david@hotmail.com>
2020-12-04 17:46:44 +0100

    Primer ejercicio terminado

---
+++ js/ejercicio1.js
@@ -0,0 +1,11 @@
+
+// A la escucha de eventos presion en tecla del teclado
+document.addEventListener('keydown', cambiaImagen);
+
+function cambiaImagen(eve){
+   console.log(eve);
+   // Insertar imagen de fondo al presionar Alt + F12
+   if(eve.altKey && eve.key == 'F12'){
+      document.getElementById('imagen').style.backgroundImage = "url(https://source.unsplash.com/random)";
+   }
+}
