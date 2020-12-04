08f77cf043b5205fdee62c4c320fbf5e2e91ac45
David Palau Morant <palau.david@hotmail.com>
2020-12-04 21:31:03 +0100

    jhkosdfjhsjhfs

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,11 +1,9 @@
 
 // A la escucha de eventos presion en tecla del teclado
-document.addEventListener('keydown', cambiaImagen);
+document.addEventListener('keydown', (eve)=>{
 
-function cambiaImagen(eve){
-   console.log(eve);
    // Insertar imagen de fondo al presionar Alt + F12
    if(eve.altKey && eve.key == 'F12'){
       document.getElementById('imagen').style.backgroundImage = "url(https://source.unsplash.com/random)";
    }
-}
+});
--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -5,10 +5,11 @@ let capa2;
 capa1 = document.getElementById("capa1");
 capa2 = document.getElementById("capa2");
 
+document.addEventListener('drag', arrastrando);
+document.addEventListener('dragend', elArrastrado);
 document.addEventListener('drag', handleDragStart);
-document.addEventListener('dragenter', elArrastrado);
-
-function handleDragStart(e) {
+document.addEventListener('dragend', elArrastrado);
+function arrastrando(e) {
     document.getElementById("capa1").style.opacity = '0.5';
 
 }
